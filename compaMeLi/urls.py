4ca239481e9ecc6d885525577817a0818ac06b55
MonodetH <monodeth@gmail.com>
2017-06-10 13:37:41 -0400

    Intento de render

--- compaMeLi/urls.py
+++ compaMeLi/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^comparador/',include('comparador.urls')),
+    url(r'^comparador/', include('comparador.urls')),
     url(r'^admin/', admin.site.urls),
 
 ]
